186996093699e1498bd1db6904eb4395481a7166
fResult <styxmaz@gmail.com>
2020-07-07 15:10:57 +0700

    deploy

---
+++ _next/static/yGqCYSGIaObVN9STH4LiB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/education":[a],"/index":[a],"/work":[a]}}("static\u002Fchunks\u002Ff0a3692a4088427e5d4dc25b18e8e587c82cef16.149ebf97655751530b48.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
